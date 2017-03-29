0925d2666e867408b9ba781d9e9738fdc60a11a5
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-03-29 10:25:03 -0600

    ya agrega listas y pendientes

---
+++ assets/js/trello.js
@@ -0,0 +1,30 @@
+var ul = document.createElement('ul');
+var titulo = document.createElement('h5');
+var input = document.createElement('input');
+var boton = document.createElement('button');
+input.setAttribute('id', 'nuevoPendiente');
+boton.setAttribute('onclick', 'agregarPendiente()');
+ul.setAttribute('id','unorderedList');
+
+function agregarLista(){
+  var listas = document.getElementById('listas');
+  var nueva = document.getElementById('listaNueva').value;
+  var linea = document.createElement('hr');
+  ul.innerHTML = nueva;
+  listas.appendChild(ul);
+  titulo.innerHTML = 'Agregar Pendiente';
+  listas.appendChild(titulo);
+  listas.appendChild(input);
+  boton.innerHTML = 'Crear Pendiente';
+  listas.appendChild(boton);
+  listas.appendChild(linea);
+
+}
+
+function agregarPendiente(){
+  var listaPendiente = document.getElementById('unorderedList');
+  var nuevoValorPendiente = document.getElementById('nuevoPendiente').value;
+  var li = document.createElement('li');
+  li.innerHTML = nuevoValorPendiente;
+  listaPendiente.appendChild(li);
+}
