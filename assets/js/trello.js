3cfd505a47e2e369431164c9ad92cbe7e29a0505
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-03-29 11:04:37 -0600

    ya crea bien las listas

--- assets/js/trello.js
+++ assets/js/trello.js
@@ -1,13 +1,18 @@
-var ul = document.createElement('ul');
-var titulo = document.createElement('h5');
-var input = document.createElement('input');
-var boton = document.createElement('button');
-input.setAttribute('id', 'nuevoPendiente');
-boton.setAttribute('onclick', 'agregarPendiente()');
-ul.setAttribute('id','unorderedList');
-
-function agregarLista(){
+// var ul = document.createElement('ul');
+// var titulo = document.createElement('h5');
+// var input = document.createElement('input');
+// var boton = document.createElement('button');
+// input.setAttribute('id', 'nuevoPendiente');
+// boton.setAttribute('onclick', 'agregarPendiente()');
+// ul.setAttribute('id','unorderedList');
   var listas = document.getElementById('listas');
+var nuevaLista = [];
+function agregarLista(){
+  var ul = document.createElement('ul');
+  var titulo = document.createElement('h5');
+  var input = document.createElement('input');
+  var boton = document.createElement('button');
+
   var nueva = document.getElementById('listaNueva').value;
   var linea = document.createElement('hr');
   ul.innerHTML = nueva;
@@ -18,10 +23,14 @@ function agregarLista(){
   boton.innerHTML = 'Crear Pendiente';
   listas.appendChild(boton);
   listas.appendChild(linea);
-
+  input.setAttribute('id', 'nuevoPendiente');
+  boton.setAttribute('onclick', 'agregarPendiente()');
+  ul.setAttribute('id','unorderedList');
+  nuevaLista = listas.value;
 }
 
 function agregarPendiente(){
+
   var listaPendiente = document.getElementById('unorderedList');
   var nuevoValorPendiente = document.getElementById('nuevoPendiente').value;
   var li = document.createElement('li');
